0b21da868ffe384c7ffdc1d4afd7754bbf7e6579
HachiYoku <6531503142@lamduan.mfu.ac.th>
2024-12-14 19:33:42 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,54 @@
+// let person = {
+//     fname: "Mg Mg",
+//     lname: "Kyaw Sawr",
+//     age: 20,
+//     married: true,
+//     family: ["U Ba", "Daw Mya" ,"Su Su"],
+//     property:{
+//         Car: "Toyota",
+//         Bike: "Kawasaki",    
+//     },
+//     doIt: function(){
+//         console.log(this.fname+this.lname+" is riding his "+this.property.Bike);
+//     }
+// }
+
+// console.log(person.doIt());
+// console.log(person.married);
+// console.log(person.family[2]);
+// console.log(person.property.Car);
+
+
+// if(person.age>=20){
+//     console.log(person.fname + person.lname + " is a young man. ");
+// };
+
+// if(person.property.Bike="Kawasaki"){
+//     console.log(person.fname+person.lname+"'s bike is " + person.property.Bike);
+// };
+
+
+// let color= "Yellow";
+// let value;
+// switch(color){
+//     case ("Yellow"): 
+//      value="It is yellow";
+//      break;
+//     case ("Green"): 
+//      value="It is Green";
+//      break;
+//     case ("Brown"): 
+//      value="It is Brown";
+//      break;
+//     case ("Red"): 
+//      value="It is Red";break;
+//     default : 
+//     value="What is the real color?";
+// }
+
+// console.log(value);
+// document.querySelector('#head').innerText=value;
+
+for(let i=0;i<100;i++){
+    console.log("Hello World"+i);
+}
